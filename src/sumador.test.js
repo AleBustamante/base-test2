28629431e4bc1e5126b6f1f1aa4711af6245e05a
Alejandro Bustamante <adbdc.u@gmail.com>
2025-05-15 11:18:51 -0400

    2

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -19,4 +19,13 @@ describe("Sumar", () => {
   it("deberia sumar dos numeros", () => {
     expect(sumar(3, 2)).toEqual(5);
   });
+  it("deberia sumar dos numeros", () => {
+    expect(sumar(3, 2)).toEqual(5);
+  });
+  it("deberia sumar dos numeros", () => {
+    expect(sumar(3, 2)).toEqual(5);
+  });
+  it("deberia sumar dos numeros", () => {
+    expect(sumar(3, 2)).toEqual(5);
+  });
 });
